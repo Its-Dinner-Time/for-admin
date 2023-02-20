3d61e36d04fd0e828222715fd55a93081c85b8d6
dinnerTime <dinnerTime.dev@gmail.com>
2023-02-21 02:47:59 +0900

    레이아웃 스타일 적용

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
-import { HeaderVue } from '@/components';
+import { Layouts } from '@/components';
 </script>
 
 <template>
-  <HeaderVue></HeaderVue>
+  <div class="app-container">
+    <header>
+      <Layouts.HeaderVue></Layouts.HeaderVue>
+    </header>
 
-  <router-view></router-view>
+    <aside>
+      <Layouts.SidebarVue></Layouts.SidebarVue>
+    </aside>
+
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
+
+<style scoped lang="scss">
+.app-container {
+  display: grid;
+  grid-template-areas:
+    'aside head'
+    'aside main';
+  grid-template-columns: minmax(100px, max-content) 1fr;
+  grid-template-rows: minmax(50px, max-content) 1fr;
+  width: minmax(100vw);
+  min-height: 100vh;
+
+  header {
+    grid-area: head;
+  }
+
+  aside {
+    grid-area: aside;
+  }
+
+  main {
+    grid-area: main;
+  }
+}
+</style>
