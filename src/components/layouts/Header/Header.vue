8da11dabef967b271cf26cd633676fc9b0cf2042
dinnerTime <dinnerTime.dev@gmail.com>
2023-02-21 02:45:54 +0900

    헤더 수정

---
+++ src/components/layouts/Header/Header.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>
+    <span class="test-header">Header</span>
+  </h1>
+</template>
+
+<style scoped lang="scss">
+@import url('./style.scss');
+</style>
